c61c4b991e3206415ef5d448552fd3a25e314684
martin <martin@siarp.de>
2011-10-04 21:02:39 +0200

    add backup to server

--- save.php
+++ save.php
@@ -1,6 +1,9 @@
 <?php
-if (@file_put_contents('./data/badminton.json', $_POST['data']) !== FALSE) {
-	echo "success";
+$bak_file_name = './data/bak/badminton_'. time() .'.json';
+if (@copy('./data/badminton.json', $bak_file_name)) {
+	if (@file_put_contents('./data/badminton.json', $_POST['data']) !== FALSE) {
+		echo "success";
+	}
 }
 else {
 	echo "error";

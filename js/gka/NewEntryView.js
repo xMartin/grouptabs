1640b42d724b20b823715847aec5a34e1e4e47c4
xMartin <xmartin@siarp.de>
2012-12-09 02:29:09 +0100

    use comboboxes for participants and payments together and add more dynamically (#6)

--- js/gka/NewEntryView.js
+++ js/gka/NewEntryView.js
@@ -2,17 +2,47 @@ define([
 	"gka/_View",
 	"dijit/form/ValidationTextBox",
 	"dijit/form/DateTextBox",
+	"./ParticipantFormWidget",
 	"dojo/text!./templates/NewEntryView.html"
-], function(_View, ValidationTextBox, DateTextBox, template){
-
-var commaSeparatorRegex = /\s*,\s*/
-var colonSeparatorRegex = /\s*:\s*/
+], function(_View, ValidationTextBox, DateTextBox, ParticipantFormWidget, template){
 
 return dojo.declare(_View, {
 	
 	templateString: template,
 	
 	name: "newEntry",
+	
+	onShow: function(){
+		this._updateParticipants()
+		this._addParticipantFormWidget()
+	},
+	
+	_updateParticipants: function(){
+		var participants = []
+		var accounts = this.app.getAccounts()
+		for(var account in accounts){
+			participants.push({id: account})
+		}
+		this._participants = participants
+	},
+	
+	_addParticipantFormWidget: function(){
+		new ParticipantFormWidget({
+			name: "participants",
+			participants: this._participants,
+			parent: this,
+			onRemoveClick: dojo.hitch(this, this._removeParticipantFormWidget)
+		}).placeAt(this.participantsNode)
+	},
+	
+	_removeParticipantFormWidget: function(widget){
+		this.participantsNode.removeChild(widget.domNode)
+		widget.destroy()
+	},
+	
+	_onPlusParticipantClick: function(){
+		this._addParticipantFormWidget()
+	},
 	
 	_onOkClick: function(){
 		this._saveEntry()
@@ -27,11 +57,17 @@ return dojo.declare(_View, {
 	
 	_saveEntry: function(){
 		var data = this.get("value")
-		var participants = data.participants.split(commaSeparatorRegex)
-		var payments = []
-		data.payments.split(commaSeparatorRegex).forEach(function(payment){
-			var p = payment.split(colonSeparatorRegex)
-			payments.push({participant: p[0], amount: parseFloat(p[1])})
+		var participants = dojo.filter(data.participants, function(participant){
+			return participant.participant
+		})
+		participants = dojo.map(participants, function(participant){
+			return participant.participant
+		})
+		var payments = dojo.filter(data.participants, function(participant){
+			return participant.participant && participant.amount
+		})
+		payments = dojo.map(payments, function(participant){
+			return {participant: participant.participant, amount: participant.amount}
 		})
 		this.app.saveEntry({
 			id: "" + new Date().getTime(),

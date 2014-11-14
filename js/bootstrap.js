5292b1c2c5e6f8e4feaea76317c9dc6f0d0ffb87
xMartin <xmartin@siarp.de>
2014-11-14 03:05:48 +0100

    remove traces of ringjs

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,9 +1,7 @@
 require.config({
   paths: {
     react: '../bower_components/react/react',
-    pouchdb: '../bower_components/pouchdb/dist/pouchdb',
-    underscore: '../bower_components/underscore/underscore',
-    ring: '../bower_components/ring/ring'
+    pouchdb: '../bower_components/pouchdb/dist/pouchdb'
   }
 });
 

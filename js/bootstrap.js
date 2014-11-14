e3df7f1aef4caa6c741422caefcf769ea530d5bc
xMartin <xmartin@siarp.de>
2014-11-14 03:15:48 +0100

    integrate fastclick library

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,11 +1,13 @@
 require.config({
   paths: {
     react: '../bower_components/react/react',
-    pouchdb: '../bower_components/pouchdb/dist/pouchdb'
+    pouchdb: '../bower_components/pouchdb/dist/pouchdb',
+    fastclick: '../bower_components/fastclick/lib/fastclick'
   }
 });
 
-require(['app/app'], function (app) {
+require(['fastclick', 'app/app'], function (FastClick, app) {
   'use strict';
+  new FastClick(document.body);
   app.init();
 });

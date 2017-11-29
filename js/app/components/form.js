97e56a04e34fdbd413d5357c937e370980a427d1
Jan-Christoph Borchardt <hey@jancborchardt.net>
2017-11-29 03:29:15 +0100

    Layout for direct transaction detail
    Signed-off-by: Jan-Christoph Borchardt <hey@jancborchardt.net>

--- js/app/components/form.js
+++ js/app/components/form.js
@@ -120,6 +120,35 @@ function (React, createReactClass, PureRenderMixin, PropTypes, ParticipantsInput
                 })
               )
             ),
+            el('div', {className: 'form-row direct-transaction'},
+              el('select', null,
+                el('option', null, 'Martin'),
+                el('option', null, 'Jan'),
+                el('option', null, 'Giorgia'),
+                el('option', null, 'Regina'),
+                el('option', null, 'Tobi'),
+                el('option', null, 'Vicy'),
+                el('option', null, '+ New participant')
+              ),
+              el('div', {className: ''},
+                el('svg', {height: 16, width: 16},
+                  el('path', {d: 'm8.5 0.5c-0.8974 0-1.3404 1.0909-0.6973 1.7168l4.7837 4.7832h-11.573c-1.3523-0.019125-1.3523 2.0191 0 2h11.572l-4.7832 4.7832c-0.98163 0.94251 0.47155 2.3957 1.4141 1.4141l6.4911-6.49c0.387-0.3878 0.391-1.0228 0-1.414l-6.4906-6.4903c-0.1883-0.1935-0.4468-0.30268-0.7168-0.3027z'})
+                ),
+                el('input', {
+                  type: 'number',
+                  placeholder: '0'
+                })
+              ),
+              el('select', null,
+                el('option', null, 'Martin'),
+                el('option', null, 'Jan'),
+                el('option', null, 'Giorgia'),
+                el('option', null, 'Regina'),
+                el('option', null, 'Tobi'),
+                el('option', null, 'Vicy'),
+                el('option', null, '+ New participant')
+              )
+            ),
             el(ParticipantsInputList, {
               ref: 'participantsInputList',
               mode: mode,

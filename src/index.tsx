3d64faf83efb50badc68f8a08f837aba064ec01f
Martin Stadler <martin@siarp.de>
2019-11-17 15:15:06 +0100

    Add service worker and remove appcache

--- src/index.tsx
+++ src/index.tsx
@@ -59,4 +59,4 @@ function hideAppLoader () {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();

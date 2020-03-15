1c71f06e1a80674eb25cab8a79e1e92b8ac1277f
xMartin <xmartin@siarp.de>
2020-03-15 13:59:22 +0100

    Update dependencies

--- src/redux/actioncreators.ts
+++ src/redux/actioncreators.ts
@@ -1,4 +1,4 @@
-import uuidv4 from 'uuid/v4';
+import { v4 as uuidv4 } from 'uuid';
 import iobject from '../lang/iobject';
 import DbManager from '../db/manager'
 import { AllState, Services } from '..';

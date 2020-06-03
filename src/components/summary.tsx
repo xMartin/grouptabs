c9221a1c0a24395c5f9cc1e3dc0ab2acc48bafa0
xMartin <xmartin@siarp.de>
2020-06-04 00:11:24 +0200

    Improve color contrast of summary in dark mode
    * Render dark text on bright account colors

--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -21,21 +21,22 @@ function formatData(accounts: Account[]) {
     return round(result);
   };
 
-  var getCssColor = function (amount: number, maxAmount: number) {
+  var getRowStyle = function (amount: number, maxAmount: number) {
     if (!maxAmount) {
-      return "transparent";
+      return { backgroundColor: "transparent" };
     }
     var color = amount < 0 ? [226, 91, 29] : [92, 226, 14];
     var opacity = Math.abs(amount) / maxAmount;
     color.push(opacity);
-    return "rgba(" + color.join(",") + ")";
+    const textColor = opacity > 0.6 ? "var(--dark-color)" : "";
+    return { backgroundColor: `rgba(${color.join(",")})`, color: textColor };
   };
 
   var maxAmount = getMaxAmount(accounts);
   var data = accounts.map(function (account) {
     return {
       amount: round(account.amount),
-      cssColor: getCssColor(account.amount, maxAmount),
+      style: getRowStyle(account.amount, maxAmount),
       participant: account.participant,
     };
   });
@@ -48,10 +49,7 @@ const Summary: FunctionComponent<Props> = ({ accounts }) => (
     <table id="balance">
       <tbody>
         {formatData(accounts).map((account) => (
-          <tr
-            key={account.participant}
-            style={{ backgroundColor: account.cssColor }}
-          >
+          <tr key={account.participant} style={account.style}>
             <th className="account">{account.participant}</th>
             <td className="amount">{account.amount}</td>
           </tr>

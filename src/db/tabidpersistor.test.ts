a1830c02db67d0b6bf3b1e05186e129c03200bc1
xMartin <xmartin@siarp.de>
2020-10-29 16:32:03 +0100

    Fix test setup
    localStorage mock seems to be part of JSDOM now

--- src/db/tabidpersistor.test.ts
+++ src/db/tabidpersistor.test.ts
@@ -9,7 +9,7 @@ beforeEach(() => {
 
 describe("loadTabIds", () => {
   it("loads a list", () => {
-    localStorage.__STORE__[KEY] = JSON.stringify(["1111111", "2222222"]);
+    localStorage.setItem(KEY, JSON.stringify(["1111111", "2222222"]));
     const tabs = loadTabIds();
     expect(tabs).toEqual(["1111111", "2222222"]);
     expect(console.error).not.toHaveBeenCalled();
@@ -21,14 +21,14 @@ describe("loadTabIds", () => {
   });
 
   it("returns empty list if no valid JSON", () => {
-    localStorage.__STORE__[KEY] = "1111111,2222222";
+    localStorage.setItem(KEY, "1111111,2222222");
     const tabs = loadTabIds();
     expect(tabs).toEqual([]);
     expect(console.error).toHaveBeenCalledTimes(2);
   });
 
   it("returns empty list if stored data is not a list", () => {
-    localStorage.__STORE__[KEY] = '"1111111,2222222"';
+    localStorage.setItem(KEY, '"1111111,2222222"');
     const tabs = loadTabIds();
     expect(tabs).toEqual([]);
     expect(console.error).toHaveBeenCalledTimes(1);
@@ -38,14 +38,14 @@ describe("loadTabIds", () => {
 describe("addTabId", () => {
   it("stores a name with previously empty store", () => {
     addTabId("1111111");
-    expect(JSON.parse(localStorage.__STORE__[KEY])).toEqual(["1111111"]);
+    expect(JSON.parse(localStorage.getItem(KEY) || "")).toEqual(["1111111"]);
     expect(console.error).not.toHaveBeenCalled();
   });
 
   it("adds a name to the existing list", () => {
-    localStorage.__STORE__[KEY] = JSON.stringify(["1111111", "2222222"]);
+    localStorage.setItem(KEY, JSON.stringify(["1111111", "2222222"]));
     addTabId("3333333");
-    expect(JSON.parse(localStorage.__STORE__[KEY])).toEqual([
+    expect(JSON.parse(localStorage.getItem(KEY) || "")).toEqual([
       "1111111",
       "2222222",
       "3333333",
@@ -54,9 +54,9 @@ describe("addTabId", () => {
   });
 
   it("does not add tab ID if it is already stored", () => {
-    localStorage.__STORE__[KEY] = JSON.stringify(["1111111", "2222222"]);
+    localStorage.setItem(KEY, JSON.stringify(["1111111", "2222222"]));
     addTabId("1111111");
-    expect(JSON.parse(localStorage.__STORE__[KEY])).toEqual([
+    expect(JSON.parse(localStorage.getItem(KEY) || "")).toEqual([
       "1111111",
       "2222222",
     ]);
@@ -72,16 +72,16 @@ describe("addTabId", () => {
   });
 
   it("overwrites existing tabs if data cannot be loaded", () => {
-    localStorage.__STORE__[KEY] = "1111111,2222222";
+    localStorage.setItem(KEY, "1111111,2222222");
     addTabId("3333333");
-    expect(JSON.parse(localStorage.__STORE__[KEY])).toEqual(["3333333"]);
+    expect(JSON.parse(localStorage.getItem(KEY) || "")).toEqual(["3333333"]);
     expect(console.error).toHaveBeenCalledTimes(2);
   });
 });
 
 describe("clearTabIds", () => {
   it("clears", () => {
-    localStorage.__STORE__[KEY] = "1111111,2222222";
+    localStorage.setItem(KEY, "1111111,2222222");
     clearTabIds();
     expect(localStorage.getItem(KEY)).toBe(null);
     expect(console.error).not.toHaveBeenCalled();

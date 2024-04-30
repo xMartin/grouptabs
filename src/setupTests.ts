2eb60c5f844b86468d72b89a956a86e411543317
xMartin <xmartin@siarp.de>
2024-04-30 15:27:57 +0200

    Update testing library and jsdom

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,1 @@
-// https://dev.to/marabesi/configuring-vitest-and-testing-library-to-work-together-4knn
-import matchers from "@testing-library/jest-dom/matchers";
-import { expect } from "vitest";
-
-expect.extend(matchers);
+import "@testing-library/jest-dom/vitest";
